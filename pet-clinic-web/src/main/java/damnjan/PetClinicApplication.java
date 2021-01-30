400d9c5a8d2d88bffe097f20933338bc2aea9932
Damnjan998 <damnjana1998@gmail.com>
2021-01-30 16:59:42 +0100

    Adding Index Page and controller. Closes #8

--- pet-clinic-web/src/main/java/damnjan/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/damnjan/PetClinicApplication.java
@@ -1,11 +1,13 @@
+package damnjan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PetClinicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetClinicApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
 
 }
---
+++ pet-clinic-web/src/main/java/damnjan/controllers/indexController.java
@@ -0,0 +1,14 @@
+package damnjan.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+
+    @GetMapping({"/", "", "index", "index.html"})
+    public String index() {
+        return "index";
+    }
+
+}
