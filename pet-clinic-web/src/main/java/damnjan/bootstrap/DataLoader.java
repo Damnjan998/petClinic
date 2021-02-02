cb222e668877e04f771730d4ce4fe8ac0bbd8812
Damnjan998 <damnjana1998@gmail.com>
2021-02-02 19:38:42 +0100

    Added Pets and contact info to Owners. Closes #33

--- pet-clinic-web/src/main/java/damnjan/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/damnjan/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package damnjan.bootstrap;
 
 import damnjan.model.Owner;
+import damnjan.model.Pet;
 import damnjan.model.PetType;
 import damnjan.model.Vet;
 import damnjan.services.OwnerService;
@@ -8,6 +9,8 @@ import damnjan.services.PetTypeService;
 import damnjan.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -36,12 +39,32 @@ public class DataLoader implements CommandLineRunner {
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
+        owner1.setAdress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("0653232123");
+
+        Pet mikesPet = new Pet();
+        mikesPet.setPetType(savedDogPetType);
+        mikesPet.setOwner(owner1);
+        mikesPet.setBirthDate(LocalDate.now());
+        mikesPet.setName("Rosco");
+        owner1.getPets().add(mikesPet);
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
+        owner1.setAdress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("065322141");
+
+        Pet fionasCat = new Pet();
+        fionasCat.setName("Rusty");
+        fionasCat.setOwner(owner2);
+        fionasCat.setBirthDate(LocalDate.now());
+        fionasCat.setPetType(savedCatPetType);
+        owner2.getPets().add(fionasCat);
 
         ownerService.save(owner2);
 
